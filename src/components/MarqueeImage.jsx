9920eb9e5b3c6580815caecd71fad8457ee95b60
Joeri <joerischenk@outlook.com>
2022-07-10 21:44:21 +0200

    updated all the logo's

--- src/components/MarqueeImage.jsx
+++ src/components/MarqueeImage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const MyComponent = ({name, logo, height}) => {
+const MyComponent = ({name, logo, margin}) => {
     return (
-        <div className="mr-20">
-            <img src={logo} alt={name} className="h-10 opacity-50 hover:opacity-75 transition-opacity duration-300"/>
+        <div className="mr-[10vw]">
+            <img src={logo} alt={name}
+                 className="h-10 opacity-50 hover:opacity-75 transition-opacity duration-300 select-none"/>
         </div>
     );
 };
--- src/container/Marquee.jsx
+++ src/container/Marquee.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Marquee from "react-fast-marquee";
 import MarqueeImage from "../components/MarqueeImage";
-import {Spotify, Google, Amazon, Dropbox} from "../assets";
+import {Spotify, Google, Amazon, Dropbox, Asana, Airbnb} from "../assets";
 
 const MyComponent = () => {
     return (
-        <Marquee speed={40} gradientWidth={0} className="flex items-center py-20">
-            <MarqueeImage logo={Google}/>
-            <MarqueeImage logo={Amazon}/>
-            <MarqueeImage logo={Dropbox}/>
+        <Marquee speed={40} gradientWidth={0}>
+            <div className="flex items-center w-full justify-between py-20">
+                <MarqueeImage logo={Google}/>
+                <MarqueeImage logo={Amazon}/>
+                <MarqueeImage logo={Dropbox}/>
+                <MarqueeImage logo={Spotify}/>
+                <MarqueeImage logo={Asana}/>
+                <MarqueeImage logo={Airbnb} margin={'10'}/>
+            </div>
         </Marquee>
     );
 };
